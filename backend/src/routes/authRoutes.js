f53b3e62d73583ead775f9b919367849e41f46de
Chom_Chom <dutta.007.dj@gmail.com>
2025-06-01 12:00:56 +0530

    Token moved from header to cookie

--- backend/src/routes/authRoutes.js
+++ backend/src/routes/authRoutes.js
@@ -39,7 +39,7 @@ router.post('/login', async (req, res) => {
     res.cookie('access_token',token,{
       httpOnly: true,
       sameSite: 'none',
-      secure: false
+      secure: true
     })
     
     res.json({
@@ -131,7 +131,7 @@ router.post('/googleSignIn', async (req,res) => {
       res.cookie('access_token',token,{
         httpOnly: true,
         sameSite: 'none',
-        secure: false
+        secure: true
       })
 
       res.status(200).json({
