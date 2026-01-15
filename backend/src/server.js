a26c44db504701420538a24275d1b87f39abc54d
Manisankar Chakrabarty <mscabc222@outlook.com>
2026-01-15 18:21:52 +0530

    fix: Add Authorization header to CORS allowed headers to fix 401 errors

--- backend/src/server.js
+++ backend/src/server.js
@@ -25,7 +25,7 @@ app.use(cors(
 {
   origin: ['https://www.thenobiasmedia.com','http://localhost:3000'],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }
 ));
